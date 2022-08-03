256c89e6242cf9fee92d58c3e433802bdad774a8
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-08-03 12:47:32 +0200

    AoC 2015 d2p1

---
+++ src/gifts.rs
@@ -0,0 +1,71 @@
+use std::cmp;
+use std::fmt::Display;
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+pub struct Gift {
+    l: usize,
+    w: usize,
+    h: usize,
+}
+
+impl Gift {
+    pub fn new(l: usize, w: usize, h: usize) -> Gift {
+        Gift { l, w, h }
+    }
+
+    pub fn get_area(&self) -> usize {
+        let base = self.l * self.w;
+        let front = self.l * self.h;
+        let side = self.w * self.h;
+
+        2 * (base + front + side)
+    }
+
+    pub fn get_smallest_side(&self) -> usize {
+        let base = self.l * self.w;
+        let front = self.l * self.h;
+        let side = self.w * self.h;
+
+        cmp::min(base, cmp::min(front, side))
+    }
+}
+
+impl FromStr for Gift {
+    type Err = GiftError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let pieces: Vec<&str> = s.split('x').collect();
+
+        if pieces.len() != 3 {
+            return Err(GiftError::SizeLengthError);
+        }
+
+        let l = pieces[0].parse::<usize>()?;
+        let w = pieces[1].parse::<usize>()?;
+        let h = pieces[2].parse::<usize>()?;
+
+        Ok(Gift::new(l, w, h))
+    }
+}
+
+#[derive(Debug)]
+pub enum GiftError {
+    ParseError(ParseIntError),
+    SizeLengthError,
+}
+
+impl Display for GiftError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            GiftError::SizeLengthError => write!(f, "invalid size"),
+            GiftError::ParseError(parse_int_error) => write!(f, "{}", parse_int_error),
+        }
+    }
+}
+
+impl From<ParseIntError> for GiftError {
+    fn from(e: ParseIntError) -> Self {
+        GiftError::ParseError(e)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use std::fs;
+use std::str::FromStr;
+
+mod gifts;
+use gifts::Gift;
 
 fn get_floor_diff(c: char) -> Result<i32, char> {
     match c {
@@ -25,7 +29,22 @@ fn get_basement(input: &str) -> Result<usize, char> {
 }
 
 fn main() {
+    // Building stuff
     let input = fs::read_to_string("./day1.txt").unwrap();
 
     println!("{}", get_basement(input.trim()).unwrap());
+
+    // Gifts stuff
+    let gifts = fs::read_to_string("./day2.txt").unwrap();
+    let mut wrapper: usize = 0;
+
+    for line in gifts.lines() {
+        let gift = Gift::from_str(line).unwrap();
+        let area = gift.get_area();
+        let smallest = gift.get_smallest_side();
+
+        wrapper += area;
+        wrapper += smallest;
+    }
+    println!("{}", wrapper);
 }
